f33a0e5a00bdd63208d9e2f94c5c5d1e0c8568e9
catfunny <77421890+catfunny@users.noreply.github.com>
2022-10-04 23:45:00 +0200

    init

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  root: './',
+  build: {
+    outDir: 'build',
+  },
+  publicDir: 'public',
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  server: {
+    port: 3005
+  },
+});
